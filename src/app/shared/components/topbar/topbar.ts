45952d881a54a4577fd2bab9100b0079da70c566
Ingeodev <ingeodev@gmail.com>
2026-08-13 07:04:51 -0500

    refactor(topbar): use ng-icons for the alerts action

--- src/app/shared/components/topbar/topbar.ts
+++ src/app/shared/components/topbar/topbar.ts
@@ -1,9 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { GlobalStore } from '../../stores/global.store';
+import { matInfoOutline } from '@ng-icons/material-symbols/outline';
+import { hugeCloudUpload } from '@ng-icons/huge-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [ NgIcon],
+  providers :[provideIcons({ matInfoOutline, hugeCloudUpload})],
   templateUrl: './topbar.html',
   styleUrl: './topbar.css',
 })
